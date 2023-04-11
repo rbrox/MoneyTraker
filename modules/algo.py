b5a010ab9a7ed10662f7537d3ce2e735b02e15a6
Rishav Bhagat <rishavsci@gmail.com>
2023-04-11 14:44:52 +0530

    definition changes

--- modules/algo.py
+++ modules/algo.py
@@ -1,4 +1,33 @@
+# we are to include herin a fraud tracing algorithm 
+# some particular ideas which we have thought of are as follows:
+# We shall use a bayesian model which will be trained on a dataset of fraudulent transactions
+# the bayesian model will be used to predict whether a transaction is fraudulent or not
+# heirachical bayesian model diagram below
 
+'''
+        Fraudulent Transaction (FT)
+               |
+        ----------------
+        |              |
+Transaction Amount   Transaction Location
+(TA)                (TL)
+        |              |
+        ----------------
+        |              |
+Cardholder Information  Cardholder Account History
+(CI)                    (CAH)
+        |              |
+        ----------------
+        |              |
+    Merchant Information       Transaction Time
+    (MI)                         (TT)
+        |              |
+        ----------------
+        |
+    IP Address (IPA)
+
+
+'''
 
 def main():
     ...
