9df843bb855f5fd40ce9ce9b4f5388eee87de4f3
Rishav Bhagat <rishavsci@gmail.com>
2023-04-12 10:07:51 +0530

    Generate fn

--- modules/algo.py
+++ modules/algo.py
@@ -33,7 +33,54 @@ def main():
     ...
     
 def is_fraud(transaction_id: int) -> bool :
-    ...
+    import pandas as pd
+import numpy as np
+from pgmpy.models import BayesianModel
+from pgmpy.estimators import MaximumLikelihoodEstimator
+
+# Load transaction data from database into a Pandas DataFrame
+transaction_data = pd.read_sql_query('SELECT transaction_id, transaction_amount, transaction_location, cardholder_information, cardholder_account_history, merchant_information, transaction_time, ip_address, is_fraudulent FROM transactions', db_connection)
+
+# Prepare data for Bayesian network learning
+# Drop irrelevant columns and convert categorical columns to numerical codes
+data = transaction_data.drop(columns=['transaction_id'])
+data['transaction_location'] = data['transaction_location'].astype('category').cat.codes
+data['cardholder_information'] = data['cardholder_information'].astype('category').cat.codes
+data['cardholder_account_history'] = data['cardholder_account_history'].astype('category').cat.codes
+data['merchant_information'] = data['merchant_information'].astype('category').cat.codes
+data['transaction_time'] = data['transaction_time'].astype('category').cat.codes
+data['ip_address'] = data['ip_address'].astype('category').cat.codes
+
+# Split data into features (X) and target variable (y)
+X = data.drop(columns=['is_fraudulent'])
+y = data['is_fraudulent']
+
+# Create BayesianModel
+model = BayesianModel()
+
+# Define edges in the BayesianModel
+model.add_edges_from([
+    ('transaction_amount', 'is_fraudulent'),
+    ('transaction_location', 'is_fraudulent'),
+    ('cardholder_information', 'is_fraudulent'),
+    ('cardholder_account_history', 'is_fraudulent'),
+    ('merchant_information', 'is_fraudulent'),
+    ('transaction_time', 'is_fraudulent'),
+    ('ip_address', 'is_fraudulent')
+])
+
+# Learn the Bayesian network structure using Maximum Likelihood Estimation
+model.fit(X, estimator=MaximumLikelihoodEstimator)
+
+# Print the learned edges in the BayesianModel
+print("Learned Bayesian Network Structure:")
+print(model.edges())
+
+# Predict the likelihood of fraud for a new transaction
+new_transaction = {'transaction_amount': 1000, 'transaction_location': 1, 'cardholder_information': 0, 'cardholder_account_history': 1, 'merchant_information': 2, 'transaction_time': 3, 'ip_address': 5}
+fraud_likelihood = model.predict_probability([new_transaction])['is_fraudulent'].values[0]
+print("Likelihood of Fraud for the New Transaction:", fraud_likelihood)
+
     
     
 if __name__ == '__main__':
--- modules/create.py
+++ modules/create.py
@@ -2,20 +2,46 @@ import pandas as pd
 import numpy as np
 
 
-# Define the data using a dictionary
-data = {
-    'Transaction_ID': [],
-    'Account Balance': []
-}
+def main():
+    data = initialise(param())
 
-# Append data points to the dictionary
+def param()-> dict:
+    print('Param module executed')
+    param = {
+        'Transaction_ID_range': [1, 100],
+        'Account_Balance_range': [1000, 1000000],
+        'Account_Number_range': [1, 9],
+    }
+    
+    return param
 
-for id in range(1, 1000):
-    bal = np.random.randint(1000, 1000000)
-    data['Transaction_ID'].append(id)
-    data['Account Balance'].append(bal)
 
-print(data)
+def initialise(param: dict)-> dict:
+    print('Initialise module executed')
+
+    #Define the data using a dictionary
+    data = {
+        'Transaction_ID': [],
+        'Account Balance': [],
+        'Account Number': [],
+    }
+    
+     
+    id_range = param['Transaction_ID_range']
+    acc_bal_range = param['Account_Balance_range']
+    acc_num_range = param['Account_Number_range']
+    
+    
+    for id in range(id_range[0], id_range[1]):
+        bal = np.random.randint(acc_bal_range[0], acc_bal_range[1])
+        acc = np.random.randint(acc_num_range[0], acc_num_range[1])
+        data['Transaction_ID'].append(id)
+        data['Account Balance'].append(bal)
+        data['Account Number'].append(acc)
+
+    print(data)
+    
+    
 # Create a DataFrame from the dictionary
 #df = pd.DataFrame(data)
 
@@ -33,3 +59,7 @@ print(data)
 
 # Export the data to a CSV file
 #df.to_csv('data.csv', index=False)
+
+
+if __name__ == '__main__':
+    main()
